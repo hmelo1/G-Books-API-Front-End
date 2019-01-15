af02d80fef438848f69422bced5e773cb45913f9
Heriberto Melo <heriberto@melo.nyc>
2019-01-15 13:27:23 -0500

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,50 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="searchInput">
+      <input placeholder="Enter title of book..." v-model="bookToSearch">
+      <button style ="btn btn-primary" v-on:click="searchBook()"/>
+    </div>
+    <div class="resultsOutput">
+      Total: {{ totalBooks }} 
+      Book Titles:
+      <div v-for="book in results">
+        <img v-if="book.volumeInfo.imageLinks" :src="book.volumeInfo.imageLinks.thumbnail" />
+        <p>
+          Title: {{book.volumeInfo.title}}
+        </p>
+        <p>
+          Subtitle: {{book.volumeInfo.subtitle}}
+        </p>
+        <p v-for="author in book.volumeInfo.authors">
+          author: {{author}}
+        </p>
+      </div>
+    </div>
   </div>
 </template>
-
+  <script src="https://unpkg.com/axios@0.12.0/dist/axios.min.js"></script>
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      results: [],
+      totalBooks: 0,
+      bookToSearch: ''
+    }
+  },
+  methods: {
+    searchBook() {
+      /* eslint-disable no-console */
+      var bookSearch = this.bookToSearch.replace(" ", "+")
+      axios.get(`https://www.googleapis.com/books/v1/volumes?q=${bookSearch}&key=AIzaSyB5EHX0bII6xf10IovVVN-5QVWZ9Ggd4wg`)
+        .then(({data}) => {
+          this.results = data.items
+          this.totalBooks = data.items.length
+        })
+      /* eslint-disable no-console */
+    }
   }
 }
 </script>
@@ -26,3 +59,7 @@ export default {
   margin-top: 60px;
 }
 </style>
+    <!--<img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+
+<!-- https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=AIzaSyB5EHX0bII6xf10IovVVN-5QVWZ9Ggd4wg -->
---
+++ src/components/search/SearchArea.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="searchArea">
+        <input id="search-bar" type="text" placeholder="Input search term here">
+        <button id="search-btn" style="btn btn-primary" >Search</button>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'searchArea',
+  data(: {
+    msg: String
+  }
+}
+</script>
